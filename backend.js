7462582eb7b71cdf117066b7b006240683239552
shob4 <JonathonBonneau@gmail.com>
2023-03-28 22:42:34 -0500

    connected to mysql. react next

--- backend.js
+++ backend.js
@@ -3,19 +3,29 @@ var mysql = require('mysql');
 // creates a connection to server using username and password
 var con = mysql.createConnection({
 				host: "localhost",
-				user: "username",
+				user: "root",
 				password: "password"
 });
 
+//con.connect(function(err) {
+//				// throws error if not connected
+//				if (err) throw err;
+//				console.log("connected!");
+//				// query database
+//				con.query(sql, function (err, result) {
+//								if (err) throw err;
+//								console.log("Result: " + result);
+//				});
+//});
+
 con.connect(function(err) {
-				// throws error if not connected
 				if (err) throw err;
 				console.log("connected!");
-				// query database
-				con.query(sql, function (err, result) {
-								if (err) throw err;
-								console.log("Result: " + result);
-				});
+});
+
+con.end(function(err) {
+				if (err) throw err;
+				console.log("ending connection");
 });
 
 console.log("hello world");
