f49ca52baf3983d95c94fcd9933c55397335210f
shob4 <JonathonBonneau@gmail.com>
2023-03-31 22:16:04 -0500

    added comments for readability

--- backend.js
+++ backend.js
@@ -1,4 +1,6 @@
+// packages needed for node.js to connect
 const mysql = require('mysql');
+// probably not needed for final product
 const prompt = require('prompt-sync')();
 
 // creates a connection to server using username and password
@@ -23,12 +25,17 @@ var con = mysql.createConnection({
 // all of these are function calls
 con.connect(function(err) {
 				if (err) throw err;
+				// getting sql from command line
+				// needs to be changed for ui
 				var sql = prompt("what sql");
+				// actually sending the sql code to server
+				// not sure why fields is necessary, but it is
 				con.query(sql, function(err, result, fields) {
 								if (err) throw err;
 								console.log(result);
 				});
 
+				// closes connection after it's done
 				con.end(function(err) {
 								if (err) throw err;
 								console.log("ending connection");
